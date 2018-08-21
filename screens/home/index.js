7b1e3d911c1d6637983f9d3939df8c9e3ce2e62b
Alec Caputo <acaputo@mail.smcvt.edu>
2018-08-20 20:41:06 -0400

    initial home screen shell (#5)

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import { Container, Header, Body, Title } from 'native-base';
-
+import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Button, Title } from 'native-base';
 export default class Home extends React.Component {
     render(){
         return(
             <Container>
                 <Header>
                     <Body>
-                    <Title>Home</Title>
+                    <Title>Recent Posts</Title>
                     </Body>
                 </Header>
-
+                <Content>
+                    <List>
+                        <ListItem thumbnail>
+                            <Left>
+                                <Thumbnail square source={require('../../assets/stowe.jpg' )} />
+                            </Left>
+                            <Body>
+                            <Text>Stowe</Text>
+                            <Text note numberOfLines={1}>3 seats open</Text>
+                            </Body>
+                            <Right>
+                                <Button transparent>
+                                    <Text>View</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem thumbnail>
+                            <Left>
+                                <Thumbnail square source={require('../../assets/magnifying-glass-vector.png' )} />
+                            </Left>
+                            <Body>
+                            <Text>Ride to Smugglers Notch</Text>
+                            <Text note numberOfLines={1}>Will pay for gas</Text>
+                            </Body>
+                            <Right>
+                                <Button transparent>
+                                    <Text>View</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                    </List>
+                </Content>
             </Container>
         );
     }
