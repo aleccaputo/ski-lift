a056c8963a42c64b73da5fe9910e903bf989b862
Alec Caputo <acaputo@mail.smcvt.edu>
2018-08-20 20:34:44 -0400

    Fake authentication (#4)
    Set up shell for real auth and a switch navigator

---
+++ screens/authLoading/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    ActivityIndicator,
+    AsyncStorage,
+    StatusBar,
+    View,
+} from 'react-native';
+
+export default class AuthLoadingScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this._bootstrapAsync();
+    }
+
+    // Fetch the token from storage then navigate to our appropriate place
+    _bootstrapAsync = async () => {
+        const userToken = await AsyncStorage.getItem('asda');
+
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+    };
+
+    render() {
+        return (
+            <View>
+                <ActivityIndicator />
+                <StatusBar barStyle="default" />
+            </View>
+        );
+    }
+}
---
+++ screens/home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Header, Body, Title } from 'native-base';
+
+export default class Home extends React.Component {
+    render(){
+        return(
+            <Container>
+                <Header>
+                    <Body>
+                    <Title>Home</Title>
+                    </Body>
+                </Header>
+
+            </Container>
+        );
+    }
+}
